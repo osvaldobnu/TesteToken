0dfb2d1268cd338c56a6c4eed6ab3ccefdf300ba
osvaldobnu <osvaldobnu@gmail.com>
2024-07-04 16:53:31 -0300

    Adicionar arquivos de projeto.

---
+++ TesteToken/Controllers/CookiesController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using TesteToken.Services;
+
+namespace TesteToken.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CookiesController : ControllerBase
+    {
+        private readonly CookieService _cookieService;
+
+        public CookiesController(CookieService cookieService)
+        {
+            _cookieService = cookieService;
+        }
+
+        // GET: api/<CookiesController>/logar
+        [HttpGet("logar")]
+        public IActionResult Logar()
+        {
+            _cookieService.CreateCookie(Response);
+            return Ok();
+        }
+
+        // GET: api/<CookiesController>/buscaDados
+        [HttpGet("buscaDados")]
+        public IActionResult BuscaDados()
+        {
+            var token = _cookieService.GetCookie(Request);
+            if (token != null)
+            {
+                return Ok($"Cookie value: {token}");
+            }
+            return NotFound("Cookie not found");
+        }
+
+        // GET: api/<CookiesController>/alteraUsuario
+        [HttpGet("alteraUsuario")]
+        public IActionResult AlteraUsuario()
+        {
+            var token = _cookieService.GetCookie(Request);
+            if (token != null)
+            {
+                return Ok($"Cookie value: {token}");
+            }
+            return NotFound("Cookie not found");
+        }
+    }
+}
---
+++ TesteToken/Services/CookieService.cs
@@ -0,0 +1,26 @@
+﻿namespace TesteToken.Services
+{
+    public class CookieService
+    {
+        public void CreateCookie(HttpResponse response)
+        {
+            var cookieOptions = new CookieOptions
+            {
+                HttpOnly = true,
+                Secure = true, // Define como true se estiver usando HTTPS
+                Expires = DateTimeOffset.Now.AddDays(1) // Define o tempo de expiração do cookie
+            };
+
+            response.Cookies.Append("token", "LALALA-LELELE-LILILI", cookieOptions);
+        }
+
+        public string GetCookie(HttpRequest request)
+        {
+            if (request.Cookies.TryGetValue("token", out var token))
+            {
+                return token;
+            }
+            return null;
+        }
+    }
+}
